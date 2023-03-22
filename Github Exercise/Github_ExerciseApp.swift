548eebcc4fb98576598bae44b9c2609b34f1ab91
Sukant Dwivedi <sukantdwivedi2000@gmail.com>
2023-03-22 20:31:57 +0530

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by SUKANT DWIVEDI on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
